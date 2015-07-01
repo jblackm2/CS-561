19492c1f540277e518031e9f427d3a11627e5108
bushj <Joel.H.Bush@Gmail.com>
2015-07-01 16:03:31 -0700

    Added a main class to test Github functionality

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/*This comment is to test on Github*/
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
